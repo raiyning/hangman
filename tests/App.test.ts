fc4eaafb718c367434f793b3d40c641402aee8bd
Raiyan Shaheen <raiyanshaheen@Raiyans-MacBook-Pro-2.local>
2023-08-22 12:30:41 +0100

    [updated] deploy workflow

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,2 +1,27 @@
 // write a test for the App.tsx file
+// test that the App renders without crashing
+// test that the App renders a heading with the name of "Playwright"
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+}
+);
+
+test('get started link', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+    
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    }   
+);
+
+
 
